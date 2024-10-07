b5429447b19d643541f8a8db4456b3f6ca3db485
Sirinut Euaungkanakul <sirinut.e@siamhos.com>
2024-10-07 13:32:56 +0700

    fix context

--- src/main/java/client/command/commands/gm4/MobpointRateCommand.java
+++ src/main/java/client/command/commands/gm4/MobpointRateCommand.java
@@ -1,32 +1,8 @@
-/*
-    This file is part of the HeavenMS MapleStory Server, commands OdinMS-based
-    Copyleft (L) 2016 - 2019 RonanLana
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation version 3 as published by
-    the Free Software Foundation. You may not use, modify or distribute
-    this program under any other version of the GNU Affero General Public
-    License.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*
-   @Author: Ronan
-*/
 package client.command.commands.gm4;
 
 import client.Character;
 import client.Client;
 import client.command.Command;
-import client.command.CommandContext;
 import tools.PacketCreator;
 
 public class MobpointRateCommand extends Command {
@@ -35,10 +11,11 @@ public class MobpointRateCommand extends Command {
     }
 
     @Override
-    public void execute(Client c, String[] params, CommandContext ctx) {
+    public void execute(Client c, String[] params) {
         Character player = c.getPlayer();
         if (params.length < 1) {
             player.yellowMessage("Syntax: !mobpoint <newrate>");
+            player.yellowMessage("Current Mob Point Rate: " + c.getWorldServer().getMobperspawnpoint());
             return;
         }
 
