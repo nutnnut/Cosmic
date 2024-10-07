7babbacf645538a68143881b597fdd0403265968
Sirinut Euaungkanakul <sirinut.e@siamhos.com>
2024-10-07 14:04:22 +0700

    fix full respawn calculation

---
+++ src/test/java/server/maps/MapleMapTest.java
@@ -0,0 +1,31 @@
+package server.maps;
+
+import org.junit.jupiter.api.Test;
+
+class MapleMapTest {
+
+    @Test
+    void randShouldIncludeEntireRange() {
+        Map<Integer, Integer> rands = new HashMap<>();
+
+        final int rounds = 100_000;
+        for (int i = 0; i < rounds; i++) {
+            int randomValue = Randomizer.rand(-5, 5);
+            rands.compute(randomValue, (k, v) -> v == null ? 0 : v + 1);
+        }
+
+        assertFalse(rands.containsKey(-6));
+        assertTrue(rands.containsKey(-5));
+        assertTrue(rands.containsKey(-4));
+        assertTrue(rands.containsKey(-3));
+        assertTrue(rands.containsKey(-2));
+        assertTrue(rands.containsKey(-1));
+        assertTrue(rands.containsKey(0));
+        assertTrue(rands.containsKey(1));
+        assertTrue(rands.containsKey(2));
+        assertTrue(rands.containsKey(3));
+        assertTrue(rands.containsKey(4));
+        assertTrue(rands.containsKey(5));
+        assertFalse(rands.containsKey(6));
+    }
+}
